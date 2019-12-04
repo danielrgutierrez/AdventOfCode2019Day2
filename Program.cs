c6748028572988250f457d9e2a2450f2928cad93
Daniel Gutierrez <daniel.r.gutierrez.94@gmail.com>
2019-12-03 23:16:43 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AdventOfCode2019Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
