83faa3c602fd878153e6349472e3f3f28704d5ec
Daniel Gutierrez <daniel.r.gutierrez.94@gmail.com>
2019-12-07 23:30:37 -0800

    Implement Part 1 solution

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2019Day2
 {
@@ -9,7 +11,26 @@ namespace AdventOfCode2019Day2
     {
         static void Main(string[] args)
         {
+            var input = new List<int>();
+            try
+            {
+                using (StreamReader sr = new StreamReader("input.txt"))
+                {
+                    string strProgram;
+                    while ((strProgram = sr.ReadLine()) != null)
+                    {
+                        input = strProgram.Split(',').Select(int.Parse).ToList();
+                    }
 
+                    RestoreProgramState(ref input);
+                    ProcessIntCodeProgram(ref input);
+                    Console.WriteLine($"Part 1: The value left at position 0 is {input[0]}.");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.Write($"Error reading the file: {e.Message}");
+            }
         }
 
         static void ProcessIntCodeProgram(ref List<int> program)
@@ -33,6 +54,25 @@ namespace AdventOfCode2019Day2
                 }
                 index += 4;
             }
+        }
+
+        static void PrintIntCodeProgram(List<int> program)
+        {
+            for (int i = 0; i < program.Count; i++)
+            {
+                if (i > 0 && i % 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+                Console.Write($"{program[i]},");
+            }
+        }
+
+        static void RestoreProgramState(ref List<int> program)
+        {
+            // before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
+            program[1] = 12;
+            program[2] = 2;
         }
     }
 }
